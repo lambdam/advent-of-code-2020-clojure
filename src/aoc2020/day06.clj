54634857f9510e3af6053f5543dd1a224d7d36de
Damien Ragoucy <dam@ramdam.io>
2021-08-05 18:24:04 +0200

    Add day 6 part 2

--- src/aoc2020/day06.clj
+++ src/aoc2020/day06.clj
@@ -1,5 +1,6 @@
 (ns aoc2020.day06
   (:require [clojure.java.io :as io]
+            [clojure.set :as set]
             [clojure.spec.alpha :as s]
             [clojure.string :as str]))
 
@@ -15,30 +16,43 @@
        slurp
        str/split-lines))
 
+(def data
+  (-> (loop [state ::content
+             result []
+             current-acc []
+             lines input]
+        (let [line (peek lines)]
+          (cond
+            ;; Final step
+            (empty? lines) (->> (conj result current-acc)
+                                (s/assert ::result))
+            ;; End of a block
+            (and (= ::content state) (str/blank? line))
+            (recur ::empty (conj result current-acc) [] (pop lines))
+            ;; In the middle of a block
+            (= ::content state)
+            (recur ::content result (conj current-acc line) (pop lines))
+            ;; In case of double empty lines
+            (and (= ::empty state) (str/blank? line))
+            (recur ::empty result current-acc (pop lines))
+            ;; Beginning of a block
+            (= ::empty state)
+            (recur ::content result (conj current-acc line) (pop lines))
+            ;; ---
+            :else (throw (Exception. "Illegal state")))))
+      reverse
+      vec))
+
 (def result-part-1
-  (->> (loop [state ::content
-              result []
-              current-acc []
-              lines input]
-         (let [line (peek lines)]
-           (cond
-             ;; Final step
-             (empty? lines) (->> (conj result current-acc)
-                                 (s/assert ::result))
-             ;; End of a block
-             (and (= ::content state) (str/blank? line))
-             (recur ::empty (conj result current-acc) [] (pop lines))
-             ;; In the middle of a block
-             (= ::content state)
-             (recur ::content result (conj current-acc line) (pop lines))
-             ;; In case of double empty lines
-             (and (= ::empty state) (str/blank? line))
-             (recur ::empty result current-acc (pop lines))
-             ;; Beginning of a block
-             (= ::empty state)
-             (recur ::content result (conj current-acc line) (pop lines))
-             ;; ---
-             :else (throw (Exception. "Illegal state")))))
+  (->> data
        (transduce
          (map #(->> (apply str %) (into #{}) count))
+         +)))
+
+(def result-part-2
+  (->> data
+       (transduce
+         (map #(->> (map set %)
+                    (apply set/intersection)
+                    count))
          +)))
